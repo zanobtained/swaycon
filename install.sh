2a3628e77ed3d7431147a75b369b1fe1cfe09506
Ahmad Fauzan <zanacquired@gmail.com>
2026-09-22 20:33:26 +0700

    remove bad installer

--- install.sh
+++
@@ -1,6 +0,0 @@
-sudo cp -a greetd/ /etc/
-sudo cp -a gtk-3.0/ /etc/
-sudo cp -a gtk-4.0/ /etc/
-sudo cp -a keyd/ /etc/
-sudo cp -a sway/ /etc/
-sudo cp -a xdg/ /etc/
--- local-install-backup.sh
+++
@@ -1,6 +0,0 @@
-cp -a -b gtk-3.0/ ~/.config/
-cp -a -b gtk-4.0/ ~/.config/
-cp -a -b sway/ ~/.config/
-cp -a -b xdg/foot ~/.config/
-cp -a -b xdg/qt6ct ~/.config/
-cp -a -b xdg/uwsm ~/.config/
